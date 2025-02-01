46af075ddd4504f9018aff5237d290c6cd89991a
Shohidur Rahman <s.rahmanrahi15@gmail.com>
2025-02-01 21:33:40 +0600

    without reading file

--- src/text_data.c
+++ src/text_data.c
@@ -4,16 +4,8 @@
 
 char words[200][20];
 void load_word_pool() {
-    FILE *file = fopen("word_pool.txt", "r");
-    if (file == NULL) {
-        perror("Failed to open file");
-        return;
-    }
-
     int i = 0;
-    while (fscanf(file, "%19s", words[i]) == 1 && i < 200) {
+    while (scanf("%19s", words[i]) == 1 && i < 200) {
         i++;
     }
-
-    fclose(file);
 }
